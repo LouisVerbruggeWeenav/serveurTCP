a7273ded56acd45b597718afbe0fa0ed5ea15886
LouisVerbruggeWeenav <louis.verbrugge@weenav.com>
2025-06-11 14:53:12 +0200

    refont serv

--- serverRelay.py
+++ serverRelay.py
@@ -1,3 +1,44 @@
+import socket
+# import mysql.connector
+# from mysql.connector import Error
+import threading
+
+
+local_host = '0.0.0.0' 
+local_port = 12345
+
+
+
+# mysql_host = 'localhost'
+# mysql_user = 'root'
+# mysql_password = 'welcome1'
+# mysql_database = 'fmc650_data'
+
+
+# def insert_data_to_mysql(data):
+#     try:
+        
+#         connection = mysql.connector.connect(
+#             host=mysql_host,
+#             user=mysql_user,
+#             password=mysql_password,
+#             database=mysql_database
+#         )
+        
+#         if connection.is_connected():
+#             cursor = connection.cursor()
+#             query = "INSERT INTO can_data (data) VALUES (%s)"
+#             cursor.execute(query, (data,))
+#             connection.commit()
+#             print(f"Data inserted into database: {data}")
+#     except Error as e:
+#         print(f"Error inserting data into MySQL: {e}")
+#     finally:
+#         if connection.is_connected():
+#             cursor.close()
+#             connection.close()
+
+
 def handle_client(client_socket):
     try:
         data = client_socket.recv(4096)
@@ -28,3 +69,24 @@ def handle_client(client_socket):
         print(f"Erreur : {e}")
     finally:
         client_socket.close()
+
+
+
+def start_tcp_server():
+    
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_socket.bind((local_host, local_port))
+    server_socket.listen(5)
+    print(f"Serveur TCP en écoute sur {local_host}:{local_port}...")
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Connexion acceptée de {client_address}")
+
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
+
+if __name__ == '__main__':
+    start_tcp_server()

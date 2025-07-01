b45d5c56722ac5b5ec1b5bab460c081d1a033500
LouisVerbruggeWeenav <louis.verbrugge@weenav.com>
2025-07-01 11:45:46 +0200

    m

--- envoieRequeteTCP.py
+++ envoieRequeteTCP.py
@@ -1,34 +1,11 @@
 
-
-import socket
+import requests
 import time
 
+for i in range(10):
+    print(f"Envoi de la requête {i+1}...")
 
-IP_VPS = '51.254.102.27'
-PORT = 12345
-server_address = (IP_VPS, PORT)
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    response = requests.get("http://127.0.0.1:5000")
+    print(response.text)
+    time.sleep(4)
 
-client_socket.connect(server_address)
-
-for i in range(1400):
-    message = f"Message {i+1} from Python client"
-    client_socket.sendall(message.encode('utf-8'))
-    if (i+1) % 20 == 0:  # Affiche un message tous les 20 envois
-        print(f"Message {i+1} envoyé avec succès.")
-    time.sleep(0.1)  # Pause pour éviter de surcharger le serveur
-
-    
-client_socket.close()
-print("Tous les messages envoyés avec succès.")
-
-
-"""
-import socket
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((IP_VPS, 12345))  # Remplace par l'adresse de ton VPS
-client.send("Salut serveur !".encode())
-response = client.recv(1024).decode()
-print(f"Réponse : {response}")
-client.close()
-"""

d89ba6c7b4f3534af7a35a2d498a7eb260500b92
LouisVerbruggeWeenav <louis.verbrugge@weenav.com>
2025-07-08 14:38:53 +0200

    chnage path to add /python or /rust

--- api_rust/src/main.rs
+++ api_rust/src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{get, web, App, HttpServer, Responder};
 
 #[get("/")]
 async fn index() -> impl Responder {
-    "Welcome to the API! rust"
+    "Welcome to the API! rust 1"
 }
 
 #[get("/hello/{name}")]
